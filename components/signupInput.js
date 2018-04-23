c16765d8c7e7123548460e8b1ca0cc0bb97ab9ef
jkuzmanovik <jordan.kuzmanovik@gmail.com>
2018-04-23 23:33:40 +0200

    added keyboardavoidingview

--- components/signupInput.js
+++ components/signupInput.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import {Text,View,TextInput,StyleSheet,} from 'react-native'
+import {Text,View,TextInput,StyleSheet,KeyboardAvoidingView,} from 'react-native'
 import {FormLabel,FormInput, FormValidationMessage,Button,} from 'react-native-elements'
 import PropTypes from 'prop-types'
 
 const Signup = (props) => (
             <View>
-                <FormLabel>Username </FormLabel>
-                <FormInput  placeholder='Please enter your Username' value = {props.userName}  onChangeText = {props.textInput('userName')} autoCapitalize = {'none'}  />
-                {checkError(props,'userName')}
-                <FormLabel>FirstName </FormLabel>
-                <FormInput  placeholder='Please enter your firstName' value = {props.firstName} onChangeText = {props.textInput('firstName')}/>
-                {checkError(props,'firstName')}
-                <FormLabel>LastName </FormLabel>
-                <FormInput  placeholder='Please enter your lastName' value = {props.lastName} onChangeText = {props.textInput('lastName')}/>
-                {checkError(props,'lastName')}
-                <FormLabel>Email </FormLabel>
-                <FormInput  placeholder='Please enter your email' value = {props.email} onChangeText = {props.textInput('email')} autoCapitalize = {'none'}/>
-                {checkError(props,'email')}
-                <FormLabel>Password </FormLabel>
-                <FormInput secureTextEntry placeholder='Please enter your passowrd' value = {props.password} onChangeText = {props.textInput('password')} autoCapitalize = {'none'}/>
-                {checkError(props,'password')}
-                {signupButton(props)}
+                <KeyboardAvoidingView behavior = 'position' keyboardVerticalOffset={55} enabled >
+                    <FormLabel>Username </FormLabel>
+                    <FormInput  placeholder='Please enter your Username' value = {props.userName}  onChangeText = {props.textInput('userName')} autoCapitalize = {'none'}  />
+                    {checkError(props,'userName')}
+                    <FormLabel>FirstName </FormLabel>
+                    <FormInput  placeholder='Please enter your firstName' value = {props.firstName} onChangeText = {props.textInput('firstName')}/>
+                    {checkError(props,'firstName')}
+                    <FormLabel>LastName </FormLabel>
+                    <FormInput  placeholder='Please enter your lastName' value = {props.lastName} onChangeText = {props.textInput('lastName')}/>
+                    {checkError(props,'lastName')}
+                    <FormLabel>Email </FormLabel>
+                    <FormInput  placeholder='Please enter your email' value = {props.email} onChangeText = {props.textInput('email')} autoCapitalize = {'none'}/>
+                    {checkError(props,'email')}
+                    <FormLabel>Password </FormLabel>
+                    <FormInput secureTextEntry placeholder='Please enter your passowrd' value = {props.password} onChangeText = {props.textInput('password')} autoCapitalize = {'none'}/>
+                    {checkError(props,'password')}
+                    {signupButton(props)}
+                </KeyboardAvoidingView>
             </View>
 )
 
